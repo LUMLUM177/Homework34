afb32a42482d69b6b7fb093422dd86e0c4e49893
LUM <lumlum177@gmail.com>
2022-11-22 13:12:10 +0300

    Начальный коммит

---
+++ src/main/java/homework34/calculator/Homework34Application.java
@@ -0,0 +1,13 @@
+package homework34.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework34Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework34Application.class, args);
+    }
+
+}
